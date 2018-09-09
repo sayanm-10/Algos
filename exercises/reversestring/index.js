65c07d141d0f1a8f9e685c9ce401985104e32ef8
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-09 14:08:53 -0400

    elegant solution using Reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,24 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// Alternate elegant solution
+// Elegant solution using Reduce
 function reverse(str) {
-    let strArr = str.split('');
+    return str.split('').reduce((reverse, char) =>  char + reverse,'');
+}    
 
-    return strArr.reverse().join('');
-}
-
-module.exports = reverse;
-
-// Solution w/o using reverse helper
+// Alternate elegant solution using Array reverse
 // function reverse(str) {
 //     let strArr = str.split('');
 
+//     return strArr.reverse().join('');
+// }
 
-//     let reverse = "";
-//     while (strArr.length > 0) {
-//         reverse += strArr.pop();
+
+// Solution 1: w/o using reverse helper
+// function reverse(str) {
+    //     let strArr = str.split('');
+    //     let reverse = "";
+
+    //     while (strArr.length > 0) {
+        //         reverse += strArr.pop();
 //     }
 
 //     return reverse;
 // }
+
+
+// Solution 2: using for-of loop
+// function reverse(str) {
+//     let reverse = '';
+    
+//     for (let character of str) {
+//         reverse = character + reverse;
+//     }
+    
+//     return reverse;
+// }
+
+module.exports = reverse;
