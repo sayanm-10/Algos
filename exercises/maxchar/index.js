84383b73c9ea2babfe6eec3c6f9bda62bb1af9d3
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-12 13:21:08 -0400

    solution for max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let arr = str.split('');
+    let map = new Map();
+
+    for (char of arr) {
+        if (map.has(char)) {
+            map.set(char, map.get(char) + 1);
+        } else {
+            map.set(char, 1);
+        }
+    }
+
+    let maxVal = map.get(arr[0]);
+    let maxChar =  arr[0];
+    map.forEach(function(val, key){
+        if (val > maxVal) {
+            maxChar = key;
+        }
+    });
+
+    return maxChar;
+}
 
 module.exports = maxChar;
