63b45d546180170adc880b857bf2534eae2cf343
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-12 13:44:34 -0400

    alternate solution using object

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,22 +5,46 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// Solution using object
 function maxChar(str) {
-    let map = new Map();
+    let map = {};
 
     for (char of str) {
-        map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+        if (map[char]) {
+            map[char]++;
+        } else {
+            map[char] = 1;
+        }
     }
 
-    let maxVal = map.get(str[0]);
     let maxChar =  str[0];
-    map.forEach(function(val, key){
-        if (val > maxVal) {
+    let maxVal = map[maxChar];
+    for (key in map) {
+        if (map[key] > maxVal) {
             maxChar = key;
         }
-    });
+    }
 
     return maxChar;
 }
+
+// Solution using Map()
+// function maxChar(str) {
+//     let map = new Map();
+
+//     for (char of str) {
+//         map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+//     }
+
+//     let maxVal = map.get(str[0]);
+//     let maxChar =  str[0];
+//     map.forEach(function(val, key){
+//         if (val > maxVal) {
+//             maxChar = key;
+//         }
+//     });
+
+//     return maxChar;
+// }
 
 module.exports = maxChar;
