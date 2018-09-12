06e43be0b3691b80375841fa7c794880e771d09e
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-12 12:34:14 -0400

    solutions for reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Without using reverse helper
+function reverseInt(n) {
+    if (n === 0) {
+        return 0;
+    }
+
+    let arrNum = n.toString().split('');
+    let trailing_zero = true;
+    let reverse = '';
+
+    for (let i = arrNum.length - 1; i >= 0; --i) {
+        if (trailing_zero && arrNum[i] === '0') {
+            // do nothing
+            continue;
+        } else {
+            reverse += arrNum[i];
+            trailing_zero = false;
+        }
+    }
+
+    return parseInt(reverse) * Math.sign(n);
+}
+
+// Using reverse() helper
+// function reverseInt(n) {
+//     let reverse = n.toString().split('').reverse().join('');
+
+//     return Math.sign(n) * parseInt(reverse);
+// }
 
 module.exports = reverseInt;
