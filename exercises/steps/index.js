d3092ffc922aef37bd37e158f7980b2c3a243f2d
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-13 12:34:52 -0400

    recursive solution for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for (let i = 1; i <= n; i++) {
-        let print = '';
-        for (let j = 1; j <= n; j++) {
-            if (j <= i) {
-                print += '#';
-            } else {
-                print += ' ';
-            }
-        }
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let print = '';
+//         for (let j = 1; j <= n; j++) {
+//             if (j <= i) {
+//                 print += '#';
+//             } else {
+//                 print += ' ';
+//             }
+//         }
 
-        console.log(print);
-    }
-}
+//         console.log(print);
+//     }
+// }
 
 module.exports = steps;
+
+// recursive solution
+function steps(n, row = 0, print = '') {
+    if (row === n) {
+        return;
+    }
+    
+    if (print.length === n) {
+        console.log(print);
+        // move on to next row
+        return steps(n, row + 1);
+    }
+
+    if (print.length <= row) {
+        print += '#';
+    } else {
+        print += ' ';
+    }
+
+    // stay in same row, work on print
+    steps(n, row, print);
+}
