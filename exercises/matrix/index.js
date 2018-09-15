33711ccea69c2b66d26c7c4ba59ff73ed1c57cbc
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-15 17:11:44 -0400

    spiral matrix solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,55 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let result = [];
+    let counter = 1;
+    let startRow = 0;
+    let startCol = 0;
+    let endRow = n - 1;
+    let endCol = n - 1;
+
+    for (let idx = 0; idx < n; idx++) {
+        result.push([]);
+    }
+
+    while (startRow <= endRow && startCol <= endCol) {
+        // L --> R
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        // T 
+        // |
+        // v
+        // B
+        for (let i = startRow; i <= endRow; i++) {
+            result[i][endCol] = counter;
+            counter++;
+        }
+        endCol--;
+
+        // L <-- R
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRow][i] = counter;
+            counter++
+        }
+        endRow--;
+
+        // T
+        // ^
+        // |
+        // B
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startCol] = counter;
+            counter++;
+        }
+        startCol++;
+    }
+
+    return result;
+}
 
 module.exports = matrix;
