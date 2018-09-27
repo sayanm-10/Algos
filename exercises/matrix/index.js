9cbbd855a3060060dd87b1ff2a104dcab24b1253
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-27 13:54:31 -0400

    revise spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -67,3 +67,46 @@ function matrix(n) {
 }
 
 module.exports = matrix;
+
+// Practice again
+
+// function matrix(n) {
+//     let result = [];
+//     let counter = 1;
+//     let startRow = 0;
+//     let startCol = 0;
+//     let endRow = n - 1;
+//     let endCol = n - 1;
+
+//     for (let j = 0; j < n; j++) {
+//         result.push([]);
+//     }
+
+//     while (startCol <= endCol && startRow <= endRow) {
+//         for(let i = startCol; i <= endCol; i++) {
+//             result[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+
+//         for(let i = startRow; i <= endRow; i++) {
+//             result[i][endCol] = counter;
+//             counter++;
+//         }
+//         endCol--;
+
+//         for(let i = endCol; i >= startCol; i--) {
+//             result[endRow][i] = counter;
+//             counter++
+//         }
+//         endRow--;
+
+//         for(let i = endRow; i >= startRow; i--) {
+//             result[i][startCol] = counter;
+//             counter++;
+//         }
+//         startCol++;
+//     }
+    
+//     return result;
+// }
