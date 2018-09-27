6b2bf6ff2155b1aa07544a1d718acdfa9d60d46f
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-09-27 14:18:42 -0400

    iterative fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,13 +1,36 @@
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
-// each number is the sum of the preceeding two.
+// each number is the sum of the preceding two.
 // For example, the sequence
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+/**
+ * * Iterative Approach
+ */
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n === 1) {
+        return 1;
+    }
+    
+    let first = 0;
+    let second = 1;
+    let result;
+
+    for (let i = 0; i < n - 1; i++) {
+        result = first + second;
+        first = second;
+        second = result;
+    }
+
+    return result;
+}
 
 module.exports = fib;
