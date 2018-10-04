2a3fea849c27ff317256b0056b9d719ddbabe887
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-10-04 14:03:30 -0400

    add initial linked list methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,81 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor (data, node = null) {
+        this.data = data;
+        this.next = node;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        const prev_head = this.head;
+        this.head = new Node(data, prev_head);
+    }
+
+    size() {
+        let size = 0;
+        let node = this.head;
+        
+        while(node) {
+            size++;
+            node = node.next;
+        }
+
+        return size;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let node = this.head;
+
+        while (node.next) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        let prev = this.head;
+        if (!prev.next) {
+            this.head = null;
+            return;
+        }
+
+        let node = prev.next;
+
+        while (node.next) {
+            prev = prev.next;
+            node = node.next;
+        }
+
+        prev.next = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
