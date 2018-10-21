c7a5b976f6ae01cda6a08c0a4240d1bdb7c336a3
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-10-21 13:34:18 -0400

    implement linked list methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -77,6 +77,93 @@ class LinkedList {
 
         prev.next = null;
     }
+
+    insertLast(newData) {
+        const newNode = new Node(newData);
+
+        // LL is empty
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            this.getLast().next = newNode;
+        }
+    }
+
+    getAt(seek) {
+        // ! Check for size makes us go through the LL twice
+        // if (seek > this.size() - 1) {
+        //     return null;
+        // }
+
+        let cur_i = 0;
+        let node = this.head;
+
+        while (node) {
+            if (cur_i === seek) {
+                return node;
+            }
+
+            cur_i++;
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    removeAt(idx) {
+        if (!this.head) {
+            return;
+        }
+
+        if (idx === 0) {
+            this.head = this.head.next;
+        }
+
+        const prev_node = this.getAt(idx - 1);
+
+        if (!prev_node || !prev_node.next) {
+            return;
+        }
+
+        prev_node.next = prev_node.next.next;
+
+        // ! Less efficient solution
+        // const next_node = this.getAt(idx + 1);
+        
+        // if (prev_node && next_node) {
+        //     prev_node.next = next_node;
+        // } else if (prev_node) {
+        //     prev_node.next = null;
+        // } else if (next_node) {
+        //     this.head = next_node;
+        // } else {
+        //     this.head = null;
+        // }
+
+
+    }
+
+    insertAt(data, index) {
+        
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+        
+        const prev_node = this.getAt(index - 1) || this.getLast();
+        const new_node = new Node(data, prev_node.next);
+        prev_node.next = new_node;
+
+
+        // if (!prev_node) {
+        //     this.insertLast(data);
+        // } else if (!prev_node.next) {
+        //     prev_node.next = new_node;
+        // } else {
+        //     new_node.next = prev_node.next;
+        //     prev_node.next = new_node;
+        // }
+    }
 }
 
 module.exports = { Node, LinkedList };
